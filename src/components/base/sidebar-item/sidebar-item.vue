13d92378301246e2b03eb2297921530273172571
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-16 22:57:41 -0300

    feat: new base component Sidebar and SidebarItem

---
+++ src/components/base/sidebar-item/sidebar-item.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <li v-if="isGroup" class="px-5" >
+            <div class="flex flex-row items-center h-8">
+                <slot />
+            </div>
+        </li>
+        <li v-else>
+            <span class="relative flex flex-row items-center h-11 focus:outline-none hover:bg-gray-50 text-gray-600 hover:text-gray-800 border-l-4 border-transparent hover:border-primary pr-6">
+                <slot />
+            </span>
+        </li>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    props: {
+        isGroup: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+    },
+    setup() {
+        return {
+
+        };
+    },
+});
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/base/sidebar/sidebar.vue
@@ -0,0 +1,43 @@
+<template>
+    <div>
+        <div class="pok-sidebar">
+            <div class="pok-sidebar--title">
+                <slot name="title" />
+            </div>
+            <div class="pok-sidebar--content">
+                <slot name="content" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    setup() {
+
+        return {
+
+        }
+
+    },
+});
+</script>
+
+<style scoped>
+
+.pok-sidebar {
+    height: 90vh;
+    @apply fixed flex flex-col top-0 left-0 w-64 bg-white border-white border-2 rounded-3xl my-8 mx-6 shadow-md
+}
+
+.pok-sidebar--title {
+    @apply flex items-center justify-center h-14
+}
+
+.pok-sidebar--content {
+    @apply overflow-y-auto overflow-x-hidden flex-grow
+}
+
+</style>
