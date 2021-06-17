7e33356f73dcdf3d8f2ab058017f4cfbdb1ec994
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-17 15:01:16 -0300

    feat: generations route

--- src/services/router.ts
+++ src/services/router.ts
@@ -1,11 +1,16 @@
 import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
 import Home from '../pages/home/home.vue'
 import NotFound from '../pages/notfound/notfound.vue'
+import Generations from '../pages/generations/generations.vue'
 
 const routes: RouteRecordRaw[] = [
     {
         path: "",
         component: Home
+    },
+    {
+        path: "/generations",
+        component: Generations
     },
     {
         path: "/:pathMatch(.*)*",
