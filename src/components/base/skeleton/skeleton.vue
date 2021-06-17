20627952e44e7428e2e63f1623ae652f7e7dbedf
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-17 15:00:18 -0300

    feat: register base skeleton as global component

---
+++ src/components/base/skeleton/skeleton.vue
@@ -0,0 +1,31 @@
+<template>
+    <div v-if="type === 'card'">
+        <Card class="my-9">
+            <template #title>
+                <div class="h-7 bg-gray-300 rounded w-1/4 animate-pulse"></div>
+            </template>
+            <template #content>
+                <div class="h-5 bg-gray-300 rounded w-3/4 my-3 animate-pulse"></div>
+                <div class="h-5 bg-gray-300 rounded w-3/4 my-3 animate-pulse"></div>
+            </template>
+        </Card>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+
+export default defineComponent({
+    props: {
+        type: {
+            type: String,
+            required: true
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+</style>
--- src/services/componentsRegistration.ts
+++ src/services/componentsRegistration.ts
@@ -1,8 +1,10 @@
 import { App } from "vue";
 import Card from '../components/base/card/card.vue'
+import Skeleton from '../components/base/skeleton/skeleton.vue'
 
 export default (app: App): void => {
 
     app.component("Card", Card)
+    app.component("Skeleton", Skeleton)
 
 }
