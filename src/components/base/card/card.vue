26332b44f3aa44cf4607038a62c4502dde09ba54
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-17 14:59:36 -0300

    feat: card base component

---
+++ src/components/base/card/card.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="card">
+        <div class="card-main">
+            <div class="flex flex-col mb-8">
+                <slot name="title" />
+            </div>
+            <div class="card--content">
+                <slot name="content" />
+            </div>
+            <div class="py-4">
+                <slot name="footer" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    name: "Card",
+    setup () {
+
+
+        return {}
+    }
+})
+</script>
+
+<style scoped>
+
+.card {
+    @apply bg-white shadow-md rounded-lg overflow-hidden mx-auto
+}
+
+.card-main {
+    @apply py-4 px-8 mt-3
+}
+
+</style>
