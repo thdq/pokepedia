7747f2e6aa007989af0d77a95df9dee9e185f402
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-17 22:49:52 -0300

    feat: register base component Chips globally

---
+++ src/components/base/chips/chips.vue
@@ -0,0 +1,69 @@
+<template>
+    <div class="mb-3 mt-1 flex flex-wrap -m-1">
+        <span v-for="(item, i) in limitedItems" :key="i" class="pok-chips">
+            {{ item[label] }}
+        </span>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from 'vue'
+
+export default defineComponent({
+    props: {
+        limit: {
+            type: Number,
+            required: false,
+            default: 0
+        },
+        label: {
+            type: String,
+            default: 'name'
+        },
+        items: {
+            type: Array,
+            required: true
+        }
+    },
+    setup (props) {
+
+
+        const limitedItems = computed(() => {
+
+            let propsList: any[] = Object.assign([], props.items).splice(0, props.limit)
+
+            const remainingCount = Object.assign([], props.items).splice(props.limit).length
+
+
+            if(propsList.length == 0){
+
+                propsList.push({
+                    name: `---`
+                })
+
+            } else if (props.items.length >= props.limit && remainingCount > 0) {
+
+                propsList.push({
+                    name: `+${remainingCount}`
+                })
+
+            }
+
+            return propsList
+        })
+
+
+        return {
+            limitedItems
+        }
+    }
+})
+</script>
+
+<style scoped>
+
+.pok-chips {
+    @apply m-1 bg-gray-200 hover:bg-gray-300 rounded-full px-2 font-bold text-sm leading-loose
+}
+
+</style>
--- src/services/componentsRegistration.ts
+++ src/services/componentsRegistration.ts
@@ -1,10 +1,12 @@
 import { App } from "vue";
 import Card from '../components/base/card/card.vue'
 import Skeleton from '../components/base/skeleton/skeleton.vue'
+import Chips from '../components/base/chips/chips.vue'
 
 export default (app: App): void => {
 
     app.component("Card", Card)
     app.component("Skeleton", Skeleton)
+    app.component("Chips", Chips)
 
 }
