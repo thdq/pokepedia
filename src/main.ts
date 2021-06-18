bb2eb3887328c7048f0db957b58002baec01f543
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-18 10:39:19 -0300

    refactor: create factory to register router

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import routes from './services/router'
 import './styles/index.css'
+import useRouter from './services/routerRegistration'
 import useGlobalComponents from './services/componentsRegistration'
 import useGlobalDirectives from './services/directivesRegistration'
 import useAxios from './services/axiosRegistration'
 
 const app = createApp(App)
 
-app.use(routes)
+useRouter(app)
 useGlobalComponents(app)
 useGlobalDirectives(app)
 useAxios(app)
---
+++ src/services/routerRegistration.ts
@@ -0,0 +1,38 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import Home from '../pages/home/home.vue'
+import NotFound from '../pages/notfound/notfound.vue'
+import Generations from '../pages/generations/generations.vue'
+import { App } from 'vue'
+
+export default (app: App): void => {
+
+    const routes: RouteRecordRaw[] = [
+        {
+            path: "",
+            component: Home
+        },
+        {
+            path: "/generations",
+            component: Generations
+        },
+        {
+            path: "/:pathMatch(.*)*",
+            redirect: "not-found"
+        },
+        {
+            path: "/not-found",
+            component: NotFound
+        }
+    ]
+
+    const router = createRouter({
+        history: createWebHashHistory(),
+        routes
+    })
+
+    app.use(router)
+
+
+}
+
+
