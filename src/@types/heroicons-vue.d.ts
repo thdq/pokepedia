e1e1d370b2b743d31b58d4ea0e67bfc4ca871c2e
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-18 11:54:38 -0300

    feat: add heroicons typescript support

---
+++ src/@types/heroicons-vue.d.ts
@@ -0,0 +1,9 @@
+declare module "@heroicons/vue/*" {
+    const content: any
+    export default content
+}
+
+declare module "@heroicons/vue/outline" {
+    export { default as LocationMarkerIcon } from "@heroicons/vue/outline/esm/LocationMarkerIcon.js"
+
+}
