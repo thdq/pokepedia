669a4441d0e3ca78284f58c5309877d9d7ac212f
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-20 16:34:20 -0300

    feat: export by destructuring the axios instance

--- src/services/axiosRegistration.ts
+++ src/services/axiosRegistration.ts
@@ -1,11 +1,15 @@
 import axios from 'axios';
 import { App } from 'vue';
 
+const instance = axios.create({
+    baseURL: "https://pokeapi.co/api/v2"
+})
+
 export default (app: App): void => {
 
-    const instance = axios.create({
-        baseURL: "https://pokeapi.co/api/v2"
-    })
-
     app.provide('$axios', instance);
+}
+
+export {
+    instance
 }
