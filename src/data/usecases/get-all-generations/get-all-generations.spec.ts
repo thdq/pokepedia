1e38f37975f25b9b812e567c3493934716e9b297
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-20 15:23:49 -0300

    test: ensure RemoteGetGenerations not calls getDetailsGeneration if axios request throws

---
+++ src/data/usecases/get-all-generations/get-all-generations.spec.ts
@@ -0,0 +1,58 @@
+import axios, { AxiosStatic } from "axios"
+import { RemoteGetGenerations } from "./get-all-generations"
+jest.mock('axios');
+
+export interface SutTypes {
+    sut: RemoteGetGenerations
+    axiosStub: jest.Mocked<typeof axios>
+}
+
+const makeAxios = (): jest.Mocked<AxiosStatic> => {
+
+    const mockedAxios = axios as jest.Mocked<typeof axios>
+
+    mockedAxios.get.mockClear().mockImplementationOnce(async () => {
+        return Promise.resolve({
+            data: {
+                results: []
+            }
+        })
+    })
+
+    return mockedAxios
+
+}
+
+const makeSut = (): SutTypes => {
+
+    const axiosStub = makeAxios()
+    const sut = new RemoteGetGenerations(axiosStub)
+
+    return {
+        sut,
+        axiosStub
+    }
+
+
+}
+
+
+describe('RemoteGetGenerations usecase', () => {
+
+    test('Should not call getDetailsGeneration if axios request throws', async () => {
+
+        const { sut, axiosStub } = makeSut()
+
+        const getDetailsGenerationSpy = jest.spyOn(sut, 'getDetailsGeneration')
+
+        jest.spyOn(axiosStub, 'get').mockImplementationOnce(async () => {
+            return Promise.reject()
+        })
+
+        await sut.getAllGenerations()
+
+        expect(getDetailsGenerationSpy).not.toHaveBeenCalled()
+
+    })
+
+})
