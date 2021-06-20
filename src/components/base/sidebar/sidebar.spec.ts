b7518a235f724b7ce4e5f7e8fb39d22ccf57ab61
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-20 18:43:00 -0300

    test: ensure Sidebar.vue containt title slot

---
+++ src/components/base/sidebar/sidebar.spec.ts
@@ -0,0 +1,27 @@
+import { mount, VueWrapper } from '@vue/test-utils'
+import Sidebar from "./sidebar.vue"
+
+const makeWrapper = (): VueWrapper<any> => {
+
+    const wrapper = mount(Sidebar, {
+        slots: {
+            title: "Any title",
+            content: "Any content"
+        }
+    })
+
+    return wrapper
+
+}
+
+describe('Sidebar base component', () => {
+
+    test('Should contain title slot', () => {
+
+        const wrapper = makeWrapper()
+
+        expect(wrapper.html()).toContain("Any title")
+
+    })
+
+})
