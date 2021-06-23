71d7646486cc30a8a8232dbf330174b7577f86f2
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-22 22:55:44 -0300

    feat: Skeleton accepts pokemon type

--- src/components/base/skeleton/skeleton.vue
+++ src/components/base/skeleton/skeleton.vue
@@ -10,6 +10,28 @@
             </template>
         </Card>
     </div>
+    <div v-else-if="type === 'pokemon'">
+        <div class="bg-white dark:bg-dark-theme pt-6 px-6 w-96 my-6 shadow-md rounded-lg">
+            <div class="relative">
+                <div class="items-center text-center mb-8">
+                    <div class="h-4 rounded-full w-full bg-green-400 animate-pulse" />
+                </div>
+
+
+                <div class="flex text-sm mt-8 gap-3 dark:text-white">
+                    <div class="h-4 rounded-full w-full bg-blue-400 animate-pulse" />
+                    <div class="h-4 rounded-full w-full bg-red-500 animate-pulse" />
+                </div>
+                <div class="flex text-sm mt-8 dark:text-white">
+                    <div class="h-4 rounded-full w-full bg-yellow-500 animate-pulse" />
+                </div>
+
+                <div class="flex py-8">
+                    <div class="h-2 bg-gray-700 w-full rounded animate-pulse"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
