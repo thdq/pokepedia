57c9ad191aab7c49f5b3f646fea5b87601395dcd
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-23 22:18:49 -0300

    chore: init en-US messages with generation page content

---
+++ src/locales/en-US.js
@@ -0,0 +1,26 @@
+export default {
+    "pages": {
+        "generations": {
+            "title": "Games generation",
+            "generation-i": "1st generation",
+            "generation-ii": "2st generation",
+            "generation-iii": "3st generation",
+            "generation-iv": "4st generation",
+            "generation-v": "5st generation",
+            "generation-vi": "6st generation",
+            "generation-vii": "7st generation",
+            "generation-viii": "8st generation",
+            "labels": {
+                "ability": "Ability",
+                "moves": "Moves",
+                "types": "Types",
+                "pokemon-species": "Pokemon species",
+
+                "buttons": {
+                    "details": "See all the details"
+                }
+            }
+        }
+
+    }
+}
