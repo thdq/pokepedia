f75c2aa87cd9f0e90c2f59188cb457acb6ba87e0
Thiago Oliveiragit <thiagooliv_@outlook.com>
2021-06-23 23:58:05 -0300

    feat: add pt-BR messages

---
+++ src/locales/pt-BR.js
@@ -0,0 +1,95 @@
+export default {
+    "pages": {
+        "generations": {
+            "title": "Gerações de jogos",
+            "generation-i": "1ª geração",
+            "generation-ii": "2ª geração",
+            "generation-iii": "3ª geração",
+            "generation-iv": "4ª geração",
+            "generation-v": "5ª geração",
+            "generation-vi": "6ª geração",
+            "generation-vii": "7ª geração",
+            "generation-viii": "8ª geração",
+            "labels": {
+                "ability": "Habilidades",
+                "moves": "Tipos de movimentos",
+                "types": "Tipos de mecânicas",
+                "pokemon-species": "Espécies de pokemon",
+
+                "buttons": {
+                    "details": "Ver todos os detalhes"
+                }
+            }
+        },
+        "generation": {
+            "generation-i": "1ª geração",
+            "generation-ii": "2ª geração",
+            "generation-iii": "3ª geração",
+            "generation-iv": "4ª geração",
+            "generation-v": "5ª geração",
+            "generation-vi": "6ª geração",
+            "generation-vii": "7ª geração",
+            "generation-viii": "8ª geração",
+            "labels": {
+                "region": "Região principal",
+                "types": "Tipos de mecânicas",
+                "general-info": "Informações gerais",
+                "versions": "Versões de jogos",
+                "error": "Ocorreu um erro ao recuperar a informação da geração"
+            },
+            "tables": {
+                "pokemon-species": {
+                    "columns": {
+                        "pokemon-species": "Espécies de pokemon"
+                    }
+                },
+                "moves": {
+                    "columns": {
+                        "moves": "Tipos de movimentos"
+                    }
+                },
+                "types": {
+                    "columns": {
+                        "types": "Tipos de mecânicas"
+                    }
+                },
+                "abilities": {
+                    "columns": {
+                        "abilities": "Habilidades"
+                    }
+                }
+            }
+        },
+        "pokedex": {
+            "error": "Um erro ocorreu ao exibir o Pokedex"
+        },
+        "notfound": {
+            "message": "Desculpe, não econtramos essa página"
+        }
+
+    },
+    "layout": {
+        "components": {
+            "sidebar-item": {
+                "labels": {
+                    "menu": "Menu",
+                    "generations": "Gerações",
+                    "pokedex": "Pokédex",
+                    "dark-mode": "Modo noturno"
+                }
+            }
+        }
+    },
+    "components": {
+        "pokemon": {
+            "labels": {
+                "weight": "Peso",
+                "height": "Altura",
+                "attack": "Ataque",
+                "defense": "Defesa",
+                "speed": "Velocidade",
+                "catch-rate": "Taxa de captura"
+            }
+        }
+    }
+}
